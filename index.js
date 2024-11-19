cf0fb26b4ea60f9f39fd6612ed63b3ecdc95c897
Nikita Ivanov <iwonow.neck@gmail.com>
2024-11-19 10:32:51 +0300

    DZ

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/browser';
+
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+
+  dsn: 'YOUR_SENTRY_DSN',
+
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+
+});
